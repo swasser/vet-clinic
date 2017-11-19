c99968f9e3412d87ecda9c8864e0984952677795
staswasserman <stanislav.wasserman@gmail.com>
2017-11-18 17:56:47 -0800

    Finished Builder Pattern tutorial. Breed and color are mandatory. Added optional fields for favorite food and assigned doctor.

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -3,10 +3,16 @@ package serenitylabs.tutorials.vetclinic.domain;
 public class Dog {
     private final String name;
     private final String breed;
+    private final String color;
+    private final String favoriteFood;
+    private final String assignedDoctor;
 
-    private Dog(String name, String breed) {
+    private Dog(String name, String breed, String color, String favoriteFood, String assignedDoctor) {
         this.name = name;
         this.breed = breed;
+        this.color = color;
+        this.favoriteFood = favoriteFood;
+        this.assignedDoctor = assignedDoctor;
     }
 
     public String getName() {
@@ -17,15 +23,36 @@ public class Dog {
         return breed;
     }
 
-    public static DogBreeder called(String name) {
+    public String getColor() {
+        return color;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public String getAssignedDoctor() {
+        return assignedDoctor;
+    }
+
+    public static WithBreed called(String name) {
         return new DogBreeder(name);
     }
 
+    interface WithBreed {
+        WithColor ofBreed(String breed);
+    }
 
+    interface WithColor{
+        DogBreeder ofColor(String color);
+    }
 
-    public static class DogBreeder {
+    public static class DogBreeder implements WithBreed, WithColor{
         private String name;
         private String breed;
+        private String color;
+        private String favoriteFood;
+        private String assignedDoctor;
 
         public DogBreeder(String name) {
             this.name = name;
@@ -36,8 +63,24 @@ public class Dog {
             return this;
         }
 
+        public DogBreeder ofColor(String color) {
+            this.color = color;
+            return this;
+        }
+
         public Dog build() {
-            return new Dog(name,breed);
+            return new Dog(name,breed,color,favoriteFood,assignedDoctor);
+        }
+
+
+        public DogBreeder withFavoriteFood(String favoriteFood) {
+            this.favoriteFood = favoriteFood;
+            return this;
+        }
+
+        public DogBreeder withAssignedDoctor(String assignedDoctor) {
+            this.assignedDoctor = assignedDoctor;
+            return this;
         }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/DogWithManadatoryAndOptionalAttributesTest.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/DogWithManadatoryAndOptionalAttributesTest.java
@@ -8,25 +8,53 @@ public class DogWithManadatoryAndOptionalAttributesTest {
     @Test
     public void can_create_a_dog_with_a_name_and_a_breed() throws Exception {
 
-        Dog doggy = Dog.called("snoopy").ofBreed("dachshund").build();
+        Dog doggy = Dog.called("snoopy").ofBreed("dachshund").ofColor("brown").build();
 
         Assert.assertEquals("snoopy", doggy.getName());
         Assert.assertEquals("dachshund", doggy.getBreed());
+        Assert.assertEquals("brown", doggy.getColor());
 
         System.out.println("Hello, my name is: "+doggy.getName());
         System.out.println("Hello, my breed is: "+doggy.getBreed());
+        System.out.println("Hello, my color is: "+doggy.getColor());
     }
 
 
     @Test
-    public void can_create_a_dog_witout_a_breed() throws Exception {
+    public void a_dog_must_have_a_breed_and_a_color() throws Exception {
 
-        Dog doggy = Dog.called("snoopy").build();
+        Dog doggy = Dog.called("snoopy").ofBreed("german shepard").ofColor("black").build();
 
         Assert.assertEquals("snoopy", doggy.getName());
-        Assert.assertEquals(null, doggy.getBreed());
+        Assert.assertEquals("german shepard", doggy.getBreed());
+        Assert.assertEquals("black", doggy.getColor());
 
         System.out.println("Hello, my name is: "+doggy.getName());
         System.out.println("Hello, my breed is: "+doggy.getBreed());
+        System.out.println("Hello, my color is: "+doggy.getColor());
+    }
+
+    @Test
+    public void a_dog_can_have_a_favoritefood_and_an_assigneddoctor() throws Exception {
+
+        Dog doggy = Dog.called("snoopy")
+                .ofBreed("german shepard")
+                .ofColor("black")
+                .withFavoriteFood("pizza")
+                .withAssignedDoctor("jenkins")
+                .build();
+
+        Assert.assertEquals("snoopy", doggy.getName());
+        Assert.assertEquals("german shepard", doggy.getBreed());
+        Assert.assertEquals("black", doggy.getColor());
+        Assert.assertEquals("pizza", doggy.getFavoriteFood());
+        Assert.assertEquals("jenkins", doggy.getAssignedDoctor());
+
+        System.out.println("Hello, my name is: "+doggy.getName());
+        System.out.println("Hello, my breed is: "+doggy.getBreed());
+        System.out.println("Hello, my color is: "+doggy.getColor());
+        System.out.println("Hello, my favorite food is: "+doggy.getFavoriteFood());
+        System.out.println("Hello, my doctor is: "+doggy.getAssignedDoctor());
+
     }
 }
